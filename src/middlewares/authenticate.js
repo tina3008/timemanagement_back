9ae2f45991da6ccc9db68e2995a8b5cb66e7d598
tina3008 <kicja_3@ukr.net>
2025-09-04 14:49:34 +0300

    Update authenticate.js

--- src/middlewares/authenticate.js
+++ src/middlewares/authenticate.js
@@ -3,21 +3,37 @@ import { SessionsCollections } from '../db/models/Session.js';
 import { UsersCollection } from '../db/models/User.js';
 
 export const authenticate = async (req, res, next) => {
-  const authHeader = req.get('Authorization');
+  // const authHeader = req.get('Authorization');
 
-  if (!authHeader) {
-    next(createHttpError(401, 'Please provide Authorization header'));
-    return;
-  }
+  // if (!authHeader) {
+  //   next(createHttpError(401, 'Please provide Authorization header'));
+  //   return;
+  // }
 
-  const bearer = authHeader.split(' ')[0];
-  const token = authHeader.split(' ')[1];
+  // const bearer = authHeader.split(' ')[0];
+  // const token = authHeader.split(' ')[1];
 
-  if (bearer !== 'Bearer' || !token) {
-    next(createHttpError(401, 'Auth header should be of type Bearer'));
-    return;
-  }
+  // if (bearer !== 'Bearer' || !token) {
+  //   next(createHttpError(401, 'Auth header should be of type Bearer'));
+  //   return;
+  // }
+  
+    let token;
 
+    // 1. пробуем достать токен из Authorization
+    const authHeader = req.get("Authorization");
+    if (authHeader?.startsWith("Bearer ")) {
+      token = authHeader.split(" ")[1];
+    }
+
+    // 2. если нет, берём из cookies
+    if (!token && req.cookies?.accessToken) {
+      token = req.cookies.accessToken;
+    }
+
+    if (!token) {
+      return next(createHttpError(401, "No access token provided"));
+    }
   const session = await SessionsCollections.findOne({ accessToken: token });
 
   if (!session) {
