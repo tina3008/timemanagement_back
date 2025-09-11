a4a6ae8ef7dfc5be006641d4b2518b65e045ff06
tina3008 <kicja_3@ukr.net>
2025-09-11 20:10:22 +0300

    Update resurses.js

--- src/validation/resurses.js
+++ src/validation/resurses.js
@@ -4,11 +4,5 @@ export const validResursesSchema = Joi.object({
   resurseName: Joi.string().min(3).max(100),
   resurse: Joi.string().min(3).max(1000),
   category: Joi.string().min(3).max(100),
-  userId: Joi.string()
-    .regex(/^[0-9a-fA-F]{24}$/)
-    .required()
-    .messages({
-      'string.pattern.base': 'userId must be a valid ObjectId',
-      'any.required': 'userId is required',
-    }),
+
 });
