9614d2a1ac86825538ba164a5205325f5f590fd5
tina3008 <kicja_3@ukr.net>
2025-09-12 18:34:10 +0300

    Update resurses.js

--- src/services/resurses.js
+++ src/services/resurses.js
@@ -4,7 +4,7 @@ import { SORT_ORDER } from '../constants/index.js';
 
 const getAllResurses = async ({
   page = 1,
-  perPage = 10,
+  perPage = 100,
   sortOrder = SORT_ORDER.ASC,
   sortBy = '_id',
   filter,
